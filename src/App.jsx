e6fffa31899c03f99030984e363b9707c1a2b2b3
Demmyjsx <ademolaabds@gmail.com>
2025-09-10 09:18:22 +0100

    done and dusted

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,29 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Axios  from "axios"
+import { useState } from "react"
 
-import {
-  GsapFrom,
-  GsapFromTo,
-  GsapScrollTrigger,
-  GsapStagger,
-  GsapText,
-  GsapTimeline,
-  GsapTo,
-  Home,
-} from "./pages";
+export default function App() {
+  const [getExcuse, setGetExcuse] = useState();
 
-const App = () => {
+  const fetchExcuse = (excuse)=>{
+      Axios.get(`https://excuser-three.vercel.app/v1/excuse/${excuse}`).then((res)=> {
+    setGetExcuse(res.data[0].excuse)
+  }).catch(()=>{
+    
+    setGetExcuse(`nothing to fetch `)
+  })
+  }
+  
   return (
-    <div className="bg-black min-h-screen w-full">
-      <Router>
-        <Routes>
-          <Route path="/gsapto" element={<GsapTo />} />
-          <Route path="/gsapfrom" element={<GsapFrom />} />
-          <Route path="/gsapfromto" element={<GsapFromTo />} />
-          <Route path="/gsaptimeline" element={<GsapTimeline />} />
-          <Route path="/gsapstagger" element={<GsapStagger />} />
-          <Route path="/gsapscrolltrigger" element={<GsapScrollTrigger />} />
-          <Route path="/gsaptext" element={<GsapText />} />
-          <Route path="/" element={<Home />} />
-        </Routes>
-      </Router>
+    <div className="flex justify-center flex-col items-center my-20">
+      <h2 className="text-3xl">select on excuse you would like to give </h2>
+      <button onClick={()=>fetchExcuse('funny')}  className="border-2 rounded-md p-3 bg-slate-400 text-gray-300 ">funny</button>
+      <button onClick={()=>fetchExcuse('gaming')}  className="border-2 rounded-md p-3 bg-slate-400 text-gray-300 ">gaming</button>
+      <button onClick={()=>fetchExcuse('developers')}  className="border-2 rounded-md p-3 bg-slate-400 text-gray-300 ">developers</button>
+      <button onClick={()=>fetchExcuse('unbelievable')}  className="border-2 rounded-md p-3 bg-slate-400 text-gray-300 ">unbelievable</button>
+      <button onClick={()=>fetchExcuse('party')}  className="border-2 rounded-md p-3 bg-slate-400 text-gray-300 ">party</button>
+      <button onClick={()=>fetchExcuse('college')}  className="border-2 rounded-md p-3 bg-slate-400 text-gray-300 ">college</button>
+      <button onClick={()=>fetchExcuse('children')}  className="border-2 rounded-md p-3 bg-slate-400 text-gray-300 ">children</button>
+      <p>{getExcuse}</p>
     </div>
-  );
-};
-
-export default App;
+  )
+}
